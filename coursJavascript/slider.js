8fc07e30e16417b7387f5d907e17e174e27b486a
ihab abadi <ihab@utopios.net>
2020-10-14 15:14:05 +0200

    cours interval javascript exemple slider

--- coursJavascript/slider.js
+++ coursJavascript/slider.js
@@ -1,10 +1,13 @@
 let direction = "R"
 const decalage = 900
 let pos = 0
+const enfant = document.querySelector(".enfant")
+const images = enfant.querySelectorAll("img")
+const maxLength = decalage * images.length + (10 * images.length)
+enfant.style.width = maxLength + "px"
 function slider() {
-    const enfant = document.querySelector(".enfant")
     pos = (direction == "R") ? pos - decalage : pos + decalage
-    if(pos == -2700) {
+    if(pos ==  -1 * decalage * images.length) {
         direction = "L"
         pos = 0
     }

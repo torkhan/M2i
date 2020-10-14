84a34878131d72b5ae4bf140d56bd30df1aa7e70
ihab abadi <ihab@utopios.net>
2020-10-14 09:20:30 +0200

    correction moprion partie 2

--- coursJavascript/morpion.js
+++ coursJavascript/morpion.js
@@ -1,17 +1,40 @@
 const morpion = document.querySelector(".morpion")
+const btnStart = document.querySelector(".btn-start")
+
 morpion.addEventListener('click',clickCase)
+btnStart.addEventListener('click', clearCase)
+
+
 let firstPlayer = true
+let nbreCaseRempli = 0
+const maxCase = 9
+
+
 function clickCase(e) {
     const maCase = e.target
     if(maCase.classList.contains("col")) {
-    //    if(firstPlayer) {
-    //     maCase.innerText = "X"
-    //    } else {
-    //        maCase.innerText = "O"
-    //    }
         if(maCase.innerText == '') {
+            //    if(firstPlayer) {
+            //     maCase.innerText = "X"
+            //    } else {
+            //        maCase.innerText = "O"
+            //    }
             maCase.innerText = (firstPlayer) ? 'X' : 'O'
             firstPlayer = !firstPlayer
+            nbreCaseRempli++
+            if(nbreCaseRempli == maxCase) {
+                btnStart.style.display = "block"
+            }
         } 
     }
+}
+
+
+function clearCase(e) {
+    const cases = document.querySelectorAll(".morpion .col")
+    for(let c of cases) {
+        c.innerText = ''
+    }
+    e.target.style.display = "none"
+    nbreCaseRempli = 0
 }

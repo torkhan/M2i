e4c08e06230e1ced68f5602c56b2816437ca0ee1
ihab abadi <ihab@utopios.net>
2020-10-19 14:15:14 +0200

    cours react component

--- cours-react/first-app/src/Home.js
+++ cours-react/first-app/src/Home.js
@@ -6,6 +6,12 @@ export class Home extends Component {
     constructor() {
         //On rappelle le constructeur du parent
         super()
+        this.personnes = [
+            {nom : 'abadi', prenom : 'ihab'},
+            {nom : 'tata', prenom : 'toto'},
+            {nom : 'titi', prenom : 'minet'},
+            {nom : 'sdfsd', prenom : 'sdf'},
+        ]
     }
     //La qui represente le rendu de notre component
     render() {
@@ -13,7 +19,10 @@ export class Home extends Component {
             //jsx
             <div>
                 <h1>Bonjour</h1>
-                <Personne />
+                {/* <Personne nom="abadi" prenom="ihab" adresse="tourcoing" />
+                <Personne nom="toto" prenom="tata" />
+                <Personne nom="titi" prenom="minet" adresse="warner" /> */}
+                {this.personnes.map((element) => (<Personne nom={element.nom} prenom={element.prenom} />))}
             </div>
         )
     }

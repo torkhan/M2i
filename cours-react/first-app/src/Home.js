a3dde70c93a997101c95d9a9688dbd06008cb12b
Cédric Leclercq <cedlec@wanadoo.fr>
2020-10-19 14:00:57 +0200

    sdfs

--- cours-react/first-app/src/Home.js
+++ cours-react/first-app/src/Home.js
@@ -1,10 +1,10 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 
 //Création d'un component en utilisant une classe qui hérite de la classe component de React
 export class Home extends Component {
     //La qui represente le rendu de notre component
     render() {
-        return(
+        return (
             //jsx
             <div>
                 <h1>Bonjour tout le monde</h1>

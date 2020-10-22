c1330122e8474a28e43242fa45f0a7b29531eaf9
Cédric Leclercq <cedlec@wanadoo.fr>
2020-10-22 16:32:33 +0200

    fdss

---
+++ cours-react/shopping-cart/src/CartItems.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Item from "./Item"
+class CartItems extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="col-12">
+                <h1 className="row">Panier</h1>
+                {this.props.carts.map(item=>(
+                    <Item updateQty={this.props.updateQty} deleteFromCart={this.props.deleteFromCart} key={item.product.id} item={item}></Item>
+                ))}
+            </div>
+         );
+    }
+}
+ 
+export default CartItems;

18fd32420c6ff4e99bfcda1c536cdaaa30f8f6c4
Cédric Leclercq <cedlec@wanadoo.fr>
2020-10-22 14:17:16 +0200

    fdsdf

---
+++ cours-react/panier/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { PanierContainer } from './panierComponents/panierContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <PanierContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ cours-react/panier/src/panierComponents/produit.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+export class Produit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+           
+         }
+    }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}

aa80ac517af6061322625ae308cb0c26d958459f
Cédric Leclercq <cedlec@wanadoo.fr>
2020-10-23 09:53:34 +0200

    sdf

---
+++ cours-react/first-app/src/todoArchi/listeTodoArchi.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export class ListeTodoArchi extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            
+        )
+    }
+}

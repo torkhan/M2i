f50b9c9283c31dd87989d9ee402c80e92586f6b9
Cédric <cedlec@wanadoo.fr>
2020-10-26 14:09:58 +0100

    dfd

--- cours-react/petite-annonces/src/components/Menu.js
+++ cours-react/petite-annonces/src/components/Menu.js
@@ -9,14 +9,14 @@ class Menu extends Component {
     render() {
         return (
             <div className="navbar navbar-expand-lg navbar-light bg-light">
-                <ul className="navbar-nav mr-auto">
+                <ul className="navbar-nav ">
                     <li className="nav-item m-2">
                        <Link to='/'>Home</Link> 
                     </li>
-                    <li className="nav-item m-2">
+                    <li className="nav-item m-2 ml-auto">
                        <Link to='/addAnnonce'>Ajouter une annonce</Link> 
                     </li>
-                    <li className="nav-item m-2">
+                    <li className="nav-item m-2 ml-auto">
                        <Link to='/favoris'>Favoris</Link> 
                     </li>
                 </ul>

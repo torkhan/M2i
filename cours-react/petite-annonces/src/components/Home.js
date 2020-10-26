7d24619d6f46c55350c79c61351db6930e07e2ae
ihab <ihab@utopios.net>
2020-10-26 16:44:57 +0100

    correction promise search

--- cours-react/petite-annonces/src/components/Home.js
+++ cours-react/petite-annonces/src/components/Home.js
@@ -46,7 +46,7 @@ class Home extends Component {
         return ( 
             <div className="container">
                 {/* <Search search={this.search}></Search> */}
-                <Search onChange={this.search} placeholder="Votre recherche" enterButton="Search" size="large" loading={this.state.loading} />
+                <Search onChange={(e) => {this.search(e.target.value)}} placeholder="Votre recherche" enterButton="Search" size="large" loading={this.state.loading} />
                 {this.state.loading ? <div style={{textAlign: 'center'}}><Spin /></div> : <Annonces annonces={this.state.annonces}></Annonces>}
                 
             </div>

def9977ac837b041618d2f7d2d853a431c6c8417
Cédric <cedlec@wanadoo.fr>
2020-10-26 16:43:44 +0100

    er

--- cours-react/petite-annonces/src/components/Home.js
+++ cours-react/petite-annonces/src/components/Home.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { annonces, getAnnonces, search } from '../services/DataService';
 import Annonces from './Annonces';
-// import Search from './Search';
+import Search from './Search';
+import {Spin} from "antd"
 
-import {Spin, Input} from "antd"
-const {Search} = Input
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -22,31 +21,16 @@ class Home extends Component {
             })
         })
     }
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return this.state != nextState || this.props != nextProps
-    }
 
     search = (text) => {
         this.setState({
-            loading : true
-        })
-        search(text).then((result) => {
-            this.setState({
-                annonces : result,
-                loading : false
-            })
-        }).catch(err=> {
-            this.setState({
-                loading : false
-            })
+            annonces : search(text)
         })
     }
-
     render() { 
         return ( 
             <div className="container">
-                {/* <Search search={this.search}></Search> */}
-                <Search onChange={this.search} placeholder="Votre recherche" enterButton="Search" size="large" loading={this.state.loading} />
+                <Search search={this.search}></Search>
                 {this.state.loading ? <div style={{textAlign: 'center'}}><Spin /></div> : <Annonces annonces={this.state.annonces}></Annonces>}
                 
             </div>

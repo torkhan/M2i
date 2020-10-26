01d6812b75e9c0354ec07a10be3725c610b8f832
ihab <ihab@utopios.net>
2020-10-26 10:06:55 +0100

    correction petites annonces partie structure et formannonce

---
+++ cours-react/petite-annonces/src/components/Favoris.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Favoris extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>
+            Favoris
+        </div> );
+    }
+}
+ 
+export default Favoris;
---
+++ cours-react/petite-annonces/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>
+            Accueil
+        </div> );
+    }
+}
+ 
+export default Home;
